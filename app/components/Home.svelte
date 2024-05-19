006bee9fb7eb6b2a573e53fb1d6ab75307c72634
Nadet LAMBI-BIDZIMOU <nadetdev@gmail.com>
2024-05-19 22:47:33 +0200

    Add action button + images list

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,27 +1,44 @@
 <page>
-    <actionBar title="Home" />
-    <gridLayout>
-        <label class="info">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
+    <actionBar>
+      <label class="title" text="NadetDev Quote App"></label>
+    </actionBar>
+    <stackLayout class="home">
+      <image src="~/images/image{imgNumber}.jpg"/>
+      <textView text={message} class="quote" />
+      <button text="Next Quote" on:tap="{onTap}" class="button"/>
+    </stackLayout>
 </page>
 
 <script lang="ts">
-    let message: string = "Blank Svelte Native App"
+    import { alert } from "@nativescript/core";
+
+    let message: string = "Welcome to NadetDev Quote App";
+    let min = 1;
+    let max = 7;
+    let imgNumber = 1;
+
+    function randomImage(){
+      imgNumber = Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    $: imgNumber ? 1 : randomImage();
+
+    //const onTap = ()=> alert('The button works good!');
+    const onTap = ()=> randomImage();
 </script>
 
 <style>
-    .info .fas {
-        color: #3A53FF;
+    .home {
+      background: #3aff96;
     }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
+    .title {
+      font-weight: bold;
+      font-size: 20px;
+    }
+    .quote {
+      color: blue;
+      font-size: 20rem;
+      font-weight: 600;
+      text-align: center;
     }
 </style>
--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,11 @@
-import { NativeScriptConfig } from '@nativescript/core';
+import { NativeScriptConfig } from "@nativescript/core";
 
 export default {
-  id: 'org.nativescript.quotesApp',
-  appPath: 'app',
-  appResourcesPath: 'App_Resources',
+  id: "me.nadetdev.quotesApp",
+  appPath: "app",
+  appResourcesPath: "App_Resources",
   android: {
-    v8Flags: '--expose_gc',
-    markingMode: 'none'
-  }
+    v8Flags: "--expose_gc",
+    markingMode: "none",
+  },
 } as NativeScriptConfig;
